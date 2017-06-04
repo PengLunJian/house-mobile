9996da9a692723a9fa64976994e7bd8588d41ea9
penglunjian <875831103@qq.com>
2017-06-05 04:19:15 +0800

    modify js code

--- js/session.page.js
+++ js/session.page.js
@@ -8,21 +8,17 @@
 function SessionPage(obj) {
     this.url = obj.url ? obj.url : "";
     this.timer = obj.timer ? obj.timer : null;
-    this.timer = obj.timer ? obj.timer : null;
     this.pageCode = obj.pageCode ? obj.pageCode : 1;
-    this.paramsObj = obj.paramsObj ? obj.paramsObj : null;
+    this.oInput = obj.oInput ? obj.oInput : $("input[name='say']");
+    this.paramsObj = obj.paramsObj ? obj.paramsObj : this.getParamsObj({
+        method: "get_msg"
+    });
 
-    this.init(".fixed")
-        .send(".send")
-        .dropDown(".session_box")
-        .historyRecord(this.url, this.paramsObj);
+    this.init(".fixed").sendMsg(".send").dropDown();
 
 }
-
 /**
- * BEGIN 设置字体大小
- * Author:PengLunJian
- * Date:2017-06-01
+ *
  * @param element
  * @returns {SessionPage}
  */
@@ -38,93 +34,67 @@ SessionPage.prototype.init = function (element) {
     });
     return this;
 }
-
 /**
- * BEGIN 设置字体大小
- * Author:PengLunJian
- * Date:2017-06-01
+ *
  * @param element
  * @returns {SessionPage}
  */
-SessionPage.prototype.send = function (element) {
+SessionPage.prototype.sendMsg = function (element) {
     var _protoObj_ = this;
-    var paramsObj = null;
-    var tempObj = $("input[name='say']");
     $(element).on("click", function () {
-        paramsObj = {
-            method: "add_sms",
-            txt: tempObj.val(),
-            content_txt: tempObj.val(),
-            from_user: "15900912480",
-            tel: localStorage.getItem("phone"),
-            to_user: "11_803",
-            data_id: "353",
-            msg_id: "22e604c7811c23586355f63f24658530"
-        };
-        var template = _protoObj_.getTemplate(paramsObj);
-        $(".session_content").append(template);
-        tempObj.val("");
-        tempObj.focus();
-        var frontHeight = $(element).outerHeight();
-        var afterHeight = $("body").outerHeight();
-        var offsetHeight = afterHeight - frontHeight;
-        $("html,body").animate({
-            scrollTop: offsetHeight
+        var msg = nim.sendText({
+            scene: 'p2p',
+            to: '11_710',
+            text: _protoObj_.oInput.val(),
+            done: sendMsgDone
         });
+        var paramsObj = _protoObj_.getParamsObj(msg);
+        _protoObj_.refreshPage(paramsObj);
+        this.oInput.val("");
+        this.oInput.focus();
+        _protoObj_.moveBottom();
         _protoObj_.addMessage(_protoObj_.url, paramsObj);
     });
     return this;
 }
-
-/**
- * BEGIN 设置字体大小
- * Author:PengLunJian
- * Date:2017-06-01
- * @param obj
- * @returns {string}
- */
-SessionPage.prototype.getTemplate = function (obj) {
-    var phone = localStorage.getItem("phone");
-    var position = obj['from_user'] == phone ? "right" : "left";
-    var template = '<li id="' + obj['msg_id'] + '" class="' + position + '"><div class="image">'
-        + '<img src="images/chat_logo.jpg"></div><div class="info">'
-        + '<p>' + obj['content_txt'] + '</p></div></li>';
-    return template;
-}
-
 /**
  *
  * @param paramsObj
- * @param type
- * @param message
  * @returns {SessionPage}
  */
-SessionPage.prototype.showStatus = function (paramsObj, type, message) {
-    var messageTemplate = "";
-    var tempObj = $("#" + paramsObj['msg_id']);
-    if ("information" == type) {
-
-    } else if ("warning" == type) {
-        messageTemplate = '<li class="line"><div class="message"><p>' + message + '</p></div></li>';
-        tempObj.addClass("fail");
-        tempObj.after(messageTemplate);
-    } else if ("error" == type) {
-
-    } else if ("question" == type) {
-
-    }
+SessionPage.prototype.refreshPage = function (paramsObj) {
+    var phone = localStorage.getItem("phone");
+    var position = paramsObj['from_user'] == phone ? "right" : "left";
+    var msg = paramsObj['content_txt'] ? paramsObj['content_txt'] : paramsObj['text'];
+    var msgId = paramsObj['msg_id'] ? paramsObj['msg_id'] : paramsObj['idClient'];
+    var template = '<li id="' + msgId + '" class="' + position + '"><div class="image">'
+        + '<img src="images/chat_logo.jpg"></div><div class="info">'
+        + '<p>' + msg + '</p></div></li>';
+    $(".session_content").append(template);
     return this;
 }
 /**
- * BEGIN 设置字体大小
- * Author:PengLunJian
- * Date:2017-06-01
+ *
+ * @param paramsObj
+ * @param message
  * @returns {SessionPage}
  */
-SessionPage.prototype.historyRecord = function (url, paramsObj) {
+SessionPage.prototype.showStatus = function (paramsObj, message) {
+    var tempObj = $("#" + paramsObj['msg_id']);
+    tempObj.addClass("error");
+    tempObj.find(".msg").html(message);
+    return this;
+}
+/**
+ *
+ * @param url
+ * @param paramsObj
+ * @returns {SessionPage}
+ */
+SessionPage.prototype.historyRecord = function (paramsObj) {
     var _protoObj_ = this;
     $.ajax({
-        url: url,
+        url: paramsObj.url,
         type: "POST",
         data: paramsObj,
         dataType: "text",
@@ -133,7 +103,7 @@ SessionPage.prototype.historyRecord = function (url, paramsObj) {
             if (data.length > 0) {
                 _protoObj_.pageCode++;
                 for (var i = 0; i < data.length; i++) {
-                    $("#load_more").after(_protoObj_.getTemplate(data[i]));
+                    _protoObj_.refreshPage(paramsObj);
                 }
             } else {
                 $("#load_more").html("全部加载完毕！");
@@ -145,11 +115,10 @@ SessionPage.prototype.historyRecord = function (url, paramsObj) {
     });
     return this;
 }
-
 /**
- * BEGIN 设置字体大小
- * Author:PengLunJian
- * Date:2017-06-01
+ *
+ * @param url
+ * @param paramsObj
  * @returns {SessionPage}
  */
 SessionPage.prototype.addMessage = function (url, paramsObj) {
@@ -160,36 +129,76 @@ SessionPage.prototype.addMessage = function (url, paramsObj) {
         data: paramsObj,
         dataType: "text",
         success: function (data) {
-            //console.log(data);
+            console.log("添加成功！");
         },
         error: function (msg) {
-            _protoObj_.showStatus(paramsObj, "warning", "服务器连接异常，请检查您的网络或联系管理员!");
+            console.log("添加失败！");
+            _protoObj_.showStatus(paramsObj, "服务器连接异常，请检查您的网络或联系管理员!");
         }
     });
     return this;
 }
-
 /**
  *
- * @param element
  * @returns {SessionPage}
  */
-SessionPage.prototype.dropDown = function (element) {
+SessionPage.prototype.dropDown = function () {
     var _protoObj_ = this;
     $(window).on("scroll", function () {
-        var fontSize = parseFloat($("html").css("fontSize"));
-        if ($(this).scrollTop() <= 0.48 * fontSize) {
+        if ($(this).scrollTop() <= 0) {
             if (_protoObj_.timer) clearInterval(_protoObj_.timer);
             _protoObj_.timer = setTimeout(function () {
-                _protoObj_.historyRecord(_protoObj_.url, {
-                    page: _protoObj_.pageCode,
-                    method: "get_msg",
-                    user_id: "11_803",
-                    data_id: 353,
-                    customer_tel: localStorage.getItem("phone")
-                });
+                _protoObj_.historyRecord(_protoObj_.getParamsObj({
+                    method: "get_msg"
+                }));
             }, 500);
         }
+    });
+    return this;
+}
+/**
+ *
+ * @param paramsObj
+ * @returns {SessionPage}
+ */
+SessionPage.prototype.pullMsg = function (paramsObj) {
+    var _protoObj_ = this;
+    _protoObj_.refreshPage(paramsObj);
+    _protoObj_.moveBottom();
+    _protoObj_.addMessage(_protoObj_.url, paramsObj);
+    return this;
+}
+/**
+ *
+ * @param paramsObj
+ * @returns {*}
+ */
+SessionPage.prototype.getParamsObj = function (paramsObj) {
+    paramsObj.method = paramsObj.method ? paramsObj.method : "";
+    // paramsObj.txt = paramsObj.txt ? paramsObj.txt : this.oInput.val();
+    paramsObj.page = paramsObj.page ? paramsObj.page : this.pageCode;
+    paramsObj.pageCode = paramsObj.pageCode ? paramsObj.pageCode : this.pageCode;
+    paramsObj.tel = paramsObj.tel ? paramsObj.tel : localStorage.getItem("phone");
+    // paramsObj.content_txt = paramsObj.content_txt ? paramsObj.content_txt : this.oInput.val();
+    // paramsObj.from_user = paramsObj.from_user ? paramsObj.from_user : localStorage.getItem("phone");
+    // paramsObj.to_user = paramsObj.to_user ? paramsObj.to_user : "11_803";
+    paramsObj.data_id = paramsObj.data_id ? paramsObj.data_id : "353";
+    // paramsObj.msg_id = paramsObj.msg_id ? paramsObj.msg_id : paramsObj['idClient'];
+    paramsObj.url = paramsObj.url ? paramsObj.url : "http://gl.2ma2.com/ashx/IMAjax.ashx";
+
+    this.paramsObj = paramsObj;
+    return this.paramsObj;
+}
+/**
+ *
+ * @returns {SessionPage}
+ */
+SessionPage.prototype.moveBottom = function () {
+    var frontHeight = $(".fixed").outerHeight();
+    var afterHeight = $("body").outerHeight();
+    var offsetHeight = afterHeight - frontHeight;
+    $("html,body").animate({
+        scrollTop: offsetHeight
     });
     return this;
 }
